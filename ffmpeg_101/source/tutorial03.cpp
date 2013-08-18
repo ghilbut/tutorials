06fe7065830e970a9b542e2bb689968600e573a9
ghilbut <ghilbut@gmail.com>
2013-08-19 00:10:33 +0900

    change 'PacketQueue' implementation to use STL queue.

--- ffmpeg_101/source/tutorial03.cpp
+++ ffmpeg_101/source/tutorial03.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <vector>
+#include <queue>
 #include <algorithm>
 extern "C" {
 #include <libavcodec/avcodec.h>
@@ -19,79 +20,57 @@ class PacketQueue
 {
 public:
     PacketQueue(void)
-        : first_(0), last_(0), mutex_(SDL_CreateMutex()), cond_(SDL_CreateCond())
-    {
+        : mutex_(SDL_CreateMutex()), cond_(SDL_CreateCond()) {
     }
-    ~PacketQueue(void)
-    {
-        AVPacketList* next = first_;
-        while (next) {
-            av_free_packet(&(next->pkt));
-            AVPacketList* tmp = next->next;
-            av_free(next);
-            next = tmp;
-        }
+    ~PacketQueue(void) {
+        flush();
     }
 
-    bool Put(AVPacket* packet)
-    {
-        if (av_dup_packet(packet) < 0) {
-            return false;
-        }
-        AVPacketList* packet_list = static_cast<AVPacketList*>(av_malloc(sizeof(AVPacketList)));
-        if (packet_list == 0) {
-            return false;
-        }
+    bool empty(void) {
+        SDL_LockMutex(mutex_);
+        const bool e = packets_.empty();
+        SDL_UnlockMutex(mutex_);
+        return e;
+    }
 
-        packet_list->pkt = *packet;
-        packet_list->next = 0;
+    AVPacket* peek(void) {
+        AVPacket* packet = 0;
 
         SDL_LockMutex(mutex_);
-
-        if (last_ == 0) {
-            first_ = packet_list;
-        } else {
-            last_->next = packet_list;
+        if (!packets_.empty()) {
+            packet = packets_.front();
         }
-        last_ = packet_list;
-
-        SDL_CondSignal(cond_);
         SDL_UnlockMutex(mutex_);
-        return true;
+
+        return packet;
     }
 
-    bool Get(AVPacket* packet)
-    {
+    void pop(void) {
         SDL_LockMutex(mutex_);
-
-        bool retVal = false;
-        while (1) {
-            if (g_quit) {
-                break;
-            }
-
-            AVPacketList* packet_list = first_;
-            if (packet_list) {
-                first_ = packet_list->next;
-                if (first_ == 0) {
-                    last_ = 0;
-                }
-                *packet = packet_list->pkt;
-                av_free(packet_list);
-                retVal = true;
-                break;
-            } else {
-                SDL_CondWait(cond_, mutex_);
-            }
-        }
-
+        AVPacket* packet = packets_.front();
+        packets_.pop();
         SDL_UnlockMutex(mutex_);
-        return retVal;
+
+        av_free_packet(packet);
+        delete packet;
+    }
+
+    void push(AVPacket* packet) {
+        av_dup_packet(packet);
+
+        SDL_LockMutex(mutex_);
+        packets_.push(packet);
+        SDL_UnlockMutex(mutex_);
+    }
+
+    void flush(void) {
+        while (!empty()) {
+            pop();
+        }
     }
 
 private:
-    AVPacketList* first_;
-    AVPacketList* last_;
+    std::queue<AVPacket*> packets_;
     SDL_mutex* mutex_;
     SDL_cond* cond_;
 };
@@ -103,37 +82,40 @@ void audio_callback(void* userdata, Uint8* stream, int len)
 {
     SDL_LockAudio();
 
-    static std::vector<uint8_t> buf;
+    static std::vector<uint8_t> buffer;
     if (len > 0) {
-        AVPacket packet;
-        av_init_packet(&packet);
-        packet.data = 0;
-        packet.size = 0;
-
-        while (buf.size() < static_cast<size_t>(len) && audio_q.Get(&packet)) {
+        while (buffer.size() < static_cast<size_t>(len)) {
             if(g_quit) {
                 return;
             }
 
-            AVCodecContext* codec_ctx = static_cast<AVCodecContext*>(userdata);
-
-            int got_frame;
-            AVFrame frame;
-            avcodec_get_frame_defaults(&frame);
-            const int bytes_decoded = avcodec_decode_audio4(codec_ctx, &frame, &got_frame, &packet);
-            av_free_packet(&packet);
-            if(bytes_decoded < 0 || got_frame == 0) {
-                continue;
+            if (audio_q.empty()) {
+                break;
             }
 
-            buf.insert(buf.end(), frame.data[0], frame.data[0] + frame.linesize[0]);
+            AVPacket* packet = audio_q.peek();
+            if (packet) {
+                int got_frame;
+                AVFrame frame;
+                avcodec_get_frame_defaults(&frame);
+                AVCodecContext* codec_ctx = static_cast<AVCodecContext*>(userdata);
+                const int bytes_decoded = avcodec_decode_audio4(codec_ctx, &frame, &got_frame, packet);
+                audio_q.pop();
+
+                if(bytes_decoded < 0 || got_frame == 0) {
+                    continue;
+                }
+                buffer.insert(buffer.end(), frame.data[0], frame.data[0] + frame.linesize[0]);
+            }
         }
         
-        const int size = std::min((int)buf.size(), len);
-        std::vector<uint8_t>::const_iterator begin = buf.begin();
-        std::vector<uint8_t>::const_iterator end = begin + size;
-        std::copy(begin, end, stream);
-        buf.erase(begin, end);
+        const int size = std::min((int)buffer.size(), len);
+        if (size > 0) {
+            std::vector<uint8_t>::const_iterator begin = buffer.begin();
+            std::vector<uint8_t>::const_iterator end = begin + size;
+            std::copy(begin, end, stream);
+            buffer.erase(begin, end);
+        }
     }
 
     SDL_UnlockAudio();
@@ -173,7 +155,7 @@ void Display(const AVCodecContext* decode_ctx, const AVFrame* frame, SDL_Overlay
 int main(int argc, char** argv)
 {
     if (argc < 2) {
-        printf("not enough arguements.\n");
+        printf("[Tutorial] Not enough arguements.\n");
         return -1;
     }
 
@@ -188,12 +170,12 @@ int main(int argc, char** argv)
     AVFormatContext* format_ctx = 0;
     const char* filepath = argv[1];
     if (avformat_open_input(&format_ctx, filepath, 0, 0) != 0) {
-        printf("Couldn't open file. : %s\n", filepath);
+        printf("[Tutorial] Couldn't open file. : %s\n", filepath);
         return -1;
     }
 
     if (avformat_find_stream_info(format_ctx, 0) < 0) {
-        printf("Couldn't find stream infomation.\n");
+        printf("[Tutorial] Couldn't find stream infomation.\n");
         avformat_close_input(&format_ctx);
         return -1;
     }
@@ -217,7 +199,7 @@ int main(int argc, char** argv)
     }
 
     if (audio_stream == -1 || video_stream == -1) {
-        printf("Couldn't find audio or video stream, or both of them.\n");
+        printf("[Tutorial] Couldn't find audio or video stream, or both of them.\n");
         avformat_close_input(&format_ctx);
         return -1;
     }
@@ -236,7 +218,7 @@ int main(int argc, char** argv)
 
     SDL_AudioSpec spec_obtained;
     if (SDL_OpenAudio(&spec_desired, &spec_obtained) != 0) {
-        printf("SLD_OpenAudio failed: %s\n", SDL_GetError());
+        printf("[Tutorial] SLD_OpenAudio failed: %s\n", SDL_GetError());
         avformat_close_input(&format_ctx);
         return -1;
     }
@@ -244,13 +226,13 @@ int main(int argc, char** argv)
     // open audio decode context
     AVCodec* audio_decoder = avcodec_find_decoder(audio_decode_ctx->codec_id);
     if (audio_decoder == 0) {
-        printf("Unsupported audio codec\n");
+        printf("[Tutorial] Unsupported audio codec\n");
         avformat_close_input(&format_ctx);
         return -1;
     }
 
     if (avcodec_open2(audio_decode_ctx, audio_decoder, 0) < 0) {
-        printf("Couldn't open audio codec.\n");
+        printf("[Tutorial] Couldn't open audio codec.\n");
         avformat_close_input(&format_ctx);
         return -1;
     }
@@ -261,13 +243,13 @@ int main(int argc, char** argv)
     AVCodecContext* video_decode_ctx = format_ctx->streams[video_stream]->codec;
     AVCodec* decoder = avcodec_find_decoder(video_decode_ctx->codec_id);
     if (decoder == 0) {
-        printf("Unsupported video codec!\n");
+        printf("[Tutorial] Unsupported video codec!\n");
         avformat_close_input(&format_ctx);
         return -1;
     }
 
     if (avcodec_open2(video_decode_ctx, decoder, 0) < 0) {
-        printf("Couldn't open video codec.\n");
+        printf("[Tutorial] Couldn't open video codec.\n");
         avformat_close_input(&format_ctx);
         return -1;
     }
@@ -275,7 +257,7 @@ int main(int argc, char** argv)
     // frame ready
     AVFrame* frame = avcodec_alloc_frame();
     if (frame == 0) {
-        printf("Couldn't allocate video frame.\n");
+        printf("[Tutorial] Couldn't allocate video frame.\n");
         avcodec_close(video_decode_ctx);
         avformat_close_input(&format_ctx);
         return -1;
@@ -284,7 +266,7 @@ int main(int argc, char** argv)
     // SDL video ready
     SDL_Surface* screen = SDL_SetVideoMode(video_decode_ctx->width, video_decode_ctx->height, 0, 0);
     if (screen == 0) {
-        printf("SDL_SetVideoMode failed: %s\n", SDL_GetError());
+        printf("[Tutorial] SDL_SetVideoMode failed: %s\n", SDL_GetError());
         avcodec_free_frame(&frame);
         avcodec_close(video_decode_ctx);
         avformat_close_input(&format_ctx);
@@ -293,7 +275,7 @@ int main(int argc, char** argv)
 
     SDL_Overlay* bmp = SDL_CreateYUVOverlay(video_decode_ctx->width, video_decode_ctx->height, SDL_YV12_OVERLAY, screen);
     if (bmp == 0) {
-        printf("SDL_CreateYUVOverlay failed: %s\n", SDL_GetError());
+        printf("[Tutorial] SDL_CreateYUVOverlay failed: %s\n", SDL_GetError());
         avcodec_free_frame(&frame);
         avcodec_close(video_decode_ctx);
         avformat_close_input(&format_ctx);
@@ -304,7 +286,9 @@ int main(int argc, char** argv)
     while (av_read_frame(format_ctx, &packet) == 0) {
 
         if (packet.stream_index == audio_stream) {
-            audio_q.Put(&packet);
+            AVPacket* p = new AVPacket();
+            av_copy_packet(p, &packet);
+            audio_q.push(p);
         } else if (packet.stream_index == video_stream) {
             int got_picture = 0;
             if (avcodec_decode_video2(video_decode_ctx, frame, &got_picture, &packet) >= 0 && got_picture != 0) {
